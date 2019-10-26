1188750366f68e6bb4430eba7ed45ab763623b4b
Wiktor Maj <w.maj@majpage.com>
2019-10-26 12:40:37 +0200

    Add pages helper.

---
+++ src/Tool/PagesHelper.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace SimpleStructure\Tool;
+
+/**
+ * Pages helper tool
+ */
+class PagesHelper
+{
+    /** @var int */
+    public $page;
+
+    /** @var int|null */
+    public $pack;
+
+    /** @var int */
+    public $offset;
+
+    /** @var int|null */
+    public $limit;
+
+    /**
+     * Construct
+     *
+     * @param int      $page page
+     * @param int|null $pack pack
+     */
+    public function __construct($page = 1, $pack = null)
+    {
+        $this->page = max(1, (int) $page);
+        if (isset($pack)) {
+            $this->limit = max(1, (int) $pack);
+            $this->offset = $this->limit * ($this->page - 1);
+        } else {
+            $this->limit = null;
+            $this->offset = 0;
+        }
+        $this->pack = $this->limit;
+    }
+
+    /**
+     * Get paginator
+     *
+     * @param array    $items       items
+     * @param int|null $totalNumber total number
+     *
+     * @return Paginator
+     */
+    public function getPaginator(array $items = [], $totalNumber = null)
+    {
+        $paginator = new Paginator($items, $this->page, $this->pack, $totalNumber);
+
+        return $paginator;
+    }
+}
---
+++ tests/Tool/PagesHelperTest.php
@@ -0,0 +1,71 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use SimpleStructure\Tool\PagesHelper;
+use SimpleStructure\Tool\Paginator;
+
+final class PagesHelperTest extends TestCase
+{
+    /**
+     * Test invalid parameters
+     */
+    public function testInvalidParameters()
+    {
+        $pagesHelper = new PagesHelper(-23, 45.7);
+        $this->assertEquals(1, $pagesHelper->page);
+        $this->assertEquals(45, $pagesHelper->pack);
+        $this->assertEquals(0, $pagesHelper->offset);
+        $this->assertEquals(45, $pagesHelper->limit);
+    }
+
+    /**
+     * Test empty pages helper
+     */
+    public function testEmptyPagesHelper()
+    {
+        $pagesHelper = new PagesHelper();
+        $this->assertEquals(1, $pagesHelper->page);
+        $this->assertEquals(null, $pagesHelper->pack);
+        $this->assertEquals(0, $pagesHelper->offset);
+        $this->assertEquals(null, $pagesHelper->limit);
+        $paginator = $pagesHelper->getPaginator();
+        $this->assertInstanceOf(Paginator::class, $paginator);
+        $this->assertEquals(1, $paginator->page);
+        $this->assertEquals(null, $paginator->pack);
+        $this->assertEquals(null, $paginator->pages);
+    }
+
+    /**
+     * Test empty paginator
+     */
+    public function testEmptyPaginator()
+    {
+        $pagesHelper = new PagesHelper(12, 20);
+        $this->assertEquals(12, $pagesHelper->page);
+        $this->assertEquals(20, $pagesHelper->pack);
+        $this->assertEquals(220, $pagesHelper->offset);
+        $this->assertEquals(20, $pagesHelper->limit);
+        $paginator = $pagesHelper->getPaginator();
+        $this->assertInstanceOf(Paginator::class, $paginator);
+        $this->assertEquals(12, $paginator->page);
+        $this->assertEquals(20, $paginator->pack);
+        $this->assertEquals(null, $paginator->pages);
+    }
+
+    /**
+     * Test custom page of paginator with total number
+     */
+    public function testCustomPageOfPaginatorWithTotalNumber()
+    {
+        $pagesHelper = new PagesHelper(3, 5);
+        $this->assertEquals(3, $pagesHelper->page);
+        $this->assertEquals(5, $pagesHelper->pack);
+        $this->assertEquals(10, $pagesHelper->offset);
+        $this->assertEquals(5, $pagesHelper->limit);
+        $paginator = $pagesHelper->getPaginator([ 1, 2, 3, 4, 5 ], 300);
+        $this->assertInstanceOf(Paginator::class, $paginator);
+        $this->assertEquals(3, $paginator->page);
+        $this->assertEquals(5, $paginator->pack);
+        $this->assertEquals(60, $paginator->pages);
+    }
+}
